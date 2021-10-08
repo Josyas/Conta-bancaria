655f945964a3c2f02fc1309bd025c80d6ea96895
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-08 20:40:08 -0300

    Add files via upload

---
+++ ContaBancaria.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Exercicio_Conta_Bancaria {
+    class ContaBancaria {
+
+        private double _numero { get; }
+        private string _titular { get; }
+        private double _quantia { get; set; }
+
+        public ContaBancaria(double numero, string titular) {
+            _numero = numero;
+            _titular = titular;
+        }  
+            
+        public void Deposito(double quantia) {
+            _quantia += quantia;
+        }
+
+        public void Saque(double quantia) {
+            _quantia -= quantia;
+            _quantia = _quantia - 5;
+        }
+
+        public override string ToString() {
+            return "Conta: " + _numero + "," + " Titular: " + _titular + ", " + "Saldo " + _quantia.ToString("F2", CultureInfo.InvariantCulture) + " R$" ;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Conta_Bancaria {
+    class Program {
+        static void Main(string[] args) {
+                                             
+            Console.Write("Entre com o número da conta: ");
+            double conta = double.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+                        
+            ContaBancaria p = new ContaBancaria(conta, titular);
+            
+            Console.Write("Haverá deposito (S/N) ");
+            string hdeposito = Console.ReadLine();
+            if (hdeposito == "S") {
+                Console.Write("Entre o valor de deposito inicial: ");
+                double qtee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                p.Deposito(qtee);
+                Console.WriteLine("Dados da conta autalizados:");
+                Console.WriteLine(p);
+            }
+            else {
+                Console.WriteLine();
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine();
+            Console.Write("Depositar: ");
+            double qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.Deposito(qte);
+            Console.WriteLine("Dados da conta autalizados:");
+            Console.WriteLine(p);
+            
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            qte = double.Parse(Console.ReadLine());
+            p.Saque(qte);
+            Console.WriteLine("Dados da conta");
+            Console.WriteLine(p);
+
+            
+            
+
+
+
+
+
+        }
+    }
+}
